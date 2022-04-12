857246b0636b6ff379094536bcf67c77f48712ed
Aditya Raj Singh <adityarajsingh64@gmail.com>
2022-04-12 09:31:10 +0530

    style: 💄 Restyled the Item Cards

--- src/components/itemCard.jsx
+++ src/components/itemCard.jsx
@@ -1,16 +1,17 @@
-import React from "react";
+import React, {useState} from "react";
 import CrudButton from "./crudButton";
+
 const ItemCard = ({ item }) => {
   return (
     <div className="flex flex-row border-2 rounded-sm w-full justify-between">
-      <div className=''>
-        <ul>
-          <li>ID: {item.id}</li>
-          <li>Name: {item.customer_name}</li>
-          <li>EMail: {item.customer_email}</li>
-          <li>Product: {item.product}</li>
-          <li>Quantity: {item.quantity}</li>
-        </ul>
+      <div>
+        <div className="text-l">
+          <div className="flex flex-row"><p className="font-bold ml-2 w-24">ID </p><p>{item.id}</p></div>
+          <div className="flex flex-row"><p className="font-bold ml-2 w-24">NAME </p><p>{item.customer_name}</p></div>
+          <div className="flex flex-row"><p className="font-bold ml-2 w-24">EMAIL </p>{item.customer_email}</div>
+          <div className="flex flex-row"><p className="font-bold ml-2 w-24">PRODUCT </p>{item.product}</div>
+          <div className="flex flex-row"><p className="font-bold ml-2 w-24">QUANTITY</p>{item.quantity}</div>
+        </div>
       </div>
       <div>
         <div className="flex flex-col items-center justify-center">
--- src/pages/home.js
+++ src/pages/home.js
@@ -1,32 +1,27 @@
 import React from "react";
 import data from "../mock/data.json";
-//import GoogleLogoutButton from "../components/googleAuth/logout";
 import Profile from "../components/profileTab";
 import ItemCard from "../components/itemCard";
 import CrudButton from "../components/crudButton";
 function Home() {
   return (
     <div className="flex flex-row">
-      {/*Container Div */}
       <div className="basis-1/5 mr-1.5">
         <Profile />
       </div>
       <div className="basis-4/5">
         <div className="flex flex-col">
-          <div>
+          <div className="h-32 border-2 relative ">
             <div className="font-serif  h-12 justify-end">
               <header className="text-center mt-3 text-3xl">
                 BUSINESS MANAGER
               </header>
             </div>
-            <div className="absolute top-10 right-5">
+            <div className="absolute bottom-1 right-5">
               <CrudButton buttonType={"ADD"} />
             </div>
           </div>
-          {/* The div with Add button */}
           <div>
-            {/*The main block for rendering data */}
-            Items
             <div>
               {data.map((item) => (
                 <div key={item.id} className="mb-2">
@@ -34,7 +29,6 @@ function Home() {
                 </div>
               ))}
             </div>
-            {/*Child div storing data, with Edit and Delete Button */}
           </div>
         </div>
       </div>
