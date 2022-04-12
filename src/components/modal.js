16d8b1b9b9ad15d24a99b771fed5567282528c9b
Aditya Raj Singh <adityarajsingh64@gmail.com>
2022-04-12 14:22:30 +0530

    fix: 🐛 Fixed the "Required" error on selecting Product option

--- src/components/modal.js
+++ src/components/modal.js
@@ -13,7 +13,7 @@ const validate = (values) => {
     } else if (!/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(values.email)) {
         errors.email = 'Invalid email address';
     }
-    if (!values.product) {
+    if (!values.product.value) {
         errors.product = 'Required';
     } else if (!['Product 1', 'Product 2', 'Product 3'].includes(values.product)) {
         errors.product = 'Invalid Product';
