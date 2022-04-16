a1676f17a5eacab029686cabe8e52949bd4ef4c8
Aditya Raj Singh <adityarajsingh64@gmail.com>
2022-04-16 19:41:45 +0530

    style: 💄 Changed colors and styles for home and login page

--- src/pages/signIn.js
+++ src/pages/signIn.js
@@ -3,8 +3,8 @@ import GoogleLoginButton from '../components/googleAuth/login';
 
 function SignIn({setToken}) {
     return (
-        <div className='flex flex-col items-center justify-center h-screen'>
-            <h1>Hello</h1>
+        <div className='flex flex-col items-center justify-center h-screen bg-amber-200'>
+            <div className='font-bold text-red-900 text-2xl mb-5'>BUSINESS MANAGER</div>
             <GoogleLoginButton setToken={setToken} />
         </div>
     );
