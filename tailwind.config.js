896136232f992795f45ca0871618971ed42f360a
Aditya Raj Singh <adityarajsingh64@gmail.com>
2022-04-16 20:47:14 +0530

    style: 💄 Changed the fontFamily values in tailwind.config

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,12 @@ module.exports = {
     "./src/components/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily:{
+        'sans': ['Roboto', 'Ubuntu'],
+        'serif': ['Georgia']
+      }
+    },
   },
   plugins: [],
 }
